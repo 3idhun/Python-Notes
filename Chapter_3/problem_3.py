8b0525a6993db798ed3880e570106262191ca7bd
3idhun <midhunmohanm2002@gmail.com>
2025-03-23 15:05:37 +0530

    New

---
+++ Chapter_3/problem_3.py
@@ -0,0 +1,16 @@
+'''
+
+Q3. WAP to detect double space in a string.
+Q4. Replace the double string from Q3 using single spaces.
+
+'''
+
+name="Midhun is  a good person."
+print(name.find("  "))
+print(name.replace("  "," "))
+
+'''
+
+find() is used to find the strings in code and replace() is used to 
+replace() the strings in code.
+'''
