a8eb790fdac62e4af2a5245b9451aa9dc71d713b
3idhun <midhunmohanm2002@gmail.com>
2025-03-24 06:43:06 +0530

    New Learnings.

---
+++ Chapter_5/problem_2.py
@@ -0,0 +1,38 @@
+'''
+
+Q2. WAP to input eight numbers from the user and display all the
+    unique numbers (once)
+
+'''
+
+
+s=set()
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+n=input("Enter number: ")
+s.add(int(n))
+
+print(s)
+
+'''
+ctrl+D is used to duplicate a line.
+
+This is the basic application of sets as we know in sets the elemets 
+are unique. While the user gives reccuring input the set shows and
+stores the unique ones.
+
+'''
+
+
