4bbcb61a2ba439113e2e1bed3dbbad302096e678
3idhun <midhunmohanm2002@gmail.com>
2025-03-24 06:43:30 +0530

    New Learnings.

---
+++ Chapter_5/problem_5.py
@@ -0,0 +1,35 @@
+'''
+
+Q5. Create an empty dictionary. Allow 4 friends to enter their
+    favourite language as value and use key as their names.
+    Assume that the names are unique.
+
+'''
+
+d={}
+name=input("Enter the name: ")
+language=input("Enter the language: ")
+d.update({name:language})
+
+name=input("Enter the name: ")
+language=input("Enter the language: ")
+d.update({name:language})
+
+name=input("Enter the name: ")
+language=input("Enter the language: ")
+d.update({name:language})
+
+name=input("Enter the name: ")
+language=input("Enter the language: ")
+d.update({name:language})
+
+print(d)
+
+
+
+'''
+
+As we know we should enter unique values as set cancels reccuring values,
+
+
+'''
