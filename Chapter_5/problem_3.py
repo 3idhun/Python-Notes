312304560d67abc33eb09c9c14ce1a92c225a0c2
3idhun <midhunmohanm2002@gmail.com>
2025-03-24 06:43:19 +0530

    New Learnings.

---
+++ Chapter_5/problem_3.py
@@ -0,0 +1,16 @@
+'''
+
+Q3. Can we have a set with '18' (str) and 18(int) as a value.
+
+'''
+s=set()
+s.add("18")
+s.add(18)
+print(s)
+
+'''
+
+Yes, we can have 18 as an integer and string
+in the same set
+
+'''
