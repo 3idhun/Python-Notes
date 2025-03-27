1f8ffb331ae14a57ccfe7fd3d1b7cc04ed5be99b
3idhun <midhunmohanm2002@gmail.com>
2025-03-27 08:46:09 +0530

    New Learnings.

--- Chapter_6/problem_3.py
+++ Chapter_6/problem_3.py
@@ -7,9 +7,33 @@ Q3. A spam comment is defined as a text containing following keywords:
 
 '''
 
-comment_=input("Enter the comment :")
+
+
+p1="Make a lot of money"
+p2="buy now"
+p3="subscribe this"
+p4="click this"
+
+message=input("Enter the comment: ")
+
+if ((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+    print("The comment is spam")
+else:
+    print("The comment is not spam")
+
+#using for loop
+
+
+'''comment_=input("Enter the comment :")
 for _ in comment_:
     if ("Make a lot of money" or "buy now" or "subscribe this" or "click this"):
         print("The comment is spam")
     else:
-        print("The comment is not spam")
+        print("The comment is not spam")'''
+
+
+'''
+
+We can use "in" to check whether the string is present in the given input. 
+
+'''
