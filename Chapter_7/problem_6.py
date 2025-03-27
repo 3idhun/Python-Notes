675d6ad45c211192fb0cb79b1a98c68858867246
3idhun <midhunmohanm2002@gmail.com>
2025-03-28 04:01:54 +0530

    New Learnings.

---
+++ Chapter_7/problem_6.py
@@ -0,0 +1,13 @@
+'''
+
+Q6. WAP to find factorial of a given number using for loop.
+
+'''
+
+n=int(input("Enter the number : "))
+product = 1
+for i in range(1,n+1):
+    product = product * i
+
+print(product)
+
