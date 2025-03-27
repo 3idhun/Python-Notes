40b46ae17f13555512deb4b68cea0d3394e05ee5
3idhun <midhunmohanm2002@gmail.com>
2025-03-27 09:18:22 +0530

    New Learnings.

---
+++ Chapter_6/problem_4.py
@@ -0,0 +1,12 @@
+'''
+
+Q4. Write a program to find whether the given username
+    contains less than 10 characters or not.
+
+'''
+
+username=str(input("Enter the username : "))
+if len(username) < 10 :
+    print("True")
+else:
+    print("False")
