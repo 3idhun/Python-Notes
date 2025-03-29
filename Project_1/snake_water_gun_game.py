a1c582bd2fab871bdeba4299ae04f49443df88f1
3idhun <midhunmohanm2002@gmail.com>
2025-03-29 17:22:39 +0530

    New Learnings.

--- Project_1/snake_water_gun_game.py
+++ Project_1/snake_water_gun_game.py
@@ -14,11 +14,11 @@ import random
 computer = random.choice([-1,0,1])
 youstr = input("Enter your choice : ")
 youDict = {"s":1,"w":-1,"g":0}
-reverseDist = {1: "Snake", -1: "Water", 0: "Gun"}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
 
 you = youDict[youstr]
 
-print(f"You choose {reverseDist[you]}\n Computer choose {reverseDist[computer]}")
+print(f"You choose {reverseDict[you]}\nComputer choose {reverseDict[computer]}")
 
 if (computer == you):
     print("Its a draw")
@@ -30,12 +30,19 @@ else:
     elif (computer == -1 and you == 0):
         print("You lose!")
 
-    if (computer == -1 and you == -1):
+    elif (computer == 1 and you == -1):
+        print("You lose!")
+
+    elif (computer == 1 and you == 0):
         print("You win!")
 
-    if (computer == -1 and you == -1):
+    elif (computer == 0 and you == -1):
         print("You win!")
 
-    if (computer == -1 and you == -1):
-        print("You win!")
+    elif (computer == 0 and you == 1):
+        print("You lose!")
+
+    else:
+        print("Something went wrong!")
+
 
