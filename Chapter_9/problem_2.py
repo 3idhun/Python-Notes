2cda80c47a1577bf698217ae703e2650538681f4
3idhun <midhunmohanm2002@gmail.com>
2025-03-31 14:24:26 +0530

    New Learnings.

--- Chapter_9/problem_2.py
+++ Chapter_9/problem_2.py
@@ -7,4 +7,25 @@ Q2. The game function in a program lets a user play game and return the score as
 
 '''
 
-    
+import random
+
+def game():
+    print("You are playing the game..")
+    score = random.randint(1, 62)
+    # Fetch the hiscore
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore!=""):
+            hiscore=int(hiscore)
+        else:
+            hiscore=0
+
+    print(f"Your score:{score}")
+    if(score>hiscore):
+        # write this hiscore to the file
+        with open("hiscore.txt", "w") as f:
+            f.write(str(score))
+
+    return score
+
+game()
