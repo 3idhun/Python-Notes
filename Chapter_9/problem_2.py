236b70a550049887bb3eac2cbe63486e04cd53d9
3idhun <midhunmohanm2002@gmail.com>
2025-03-31 13:55:50 +0530

    New Learnings.

---
+++ Chapter_9/problem_2.py
@@ -0,0 +1,10 @@
+'''
+
+Q2. The game function in a program lets a user play game and return the score as an
+    integer. You need to read a file "Hi-score.txt" which is either blank or contains
+    the previous Hi-score. You need to WAP to update the Hi-score whenever the game()
+    function breaks the Hi-score.
+
+'''
+
+    
