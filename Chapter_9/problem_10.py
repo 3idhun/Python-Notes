65150879ba662f15c1cc5ed8628b66124fda1f7d
3idhun <midhunmohanm2002@gmail.com>
2025-04-01 23:17:01 +0530

    New Learnings.

---
+++ Chapter_9/problem_10.py
@@ -0,0 +1,8 @@
+'''
+
+Q10. WAP to wipe out the content of the file using python.
+
+'''
+
+with open("Donkee_copy.txt", "w") as f:
+    f.write("") #just writing with blank inverted commas wipes out the entire content.
