1105d17870979efadd23455b68d9970c6be682bd
3idhun <midhunmohanm2002@gmail.com>
2025-04-10 04:39:40 +0530

    New Learnings.

---
+++ Chapter_11/problem_3.py
@@ -0,0 +1,12 @@
+'''
+
+Q3. WAP to create a class 'Employee' and add salary increment properties
+    to it.
+
+    Write a method 'salaryAfterincrement' method with a @property decorator
+    with a setter which changes the value of increment based on the salary.
+
+'''
+
+class Salary:
+    
