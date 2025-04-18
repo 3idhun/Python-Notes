f438721f98ec90549c53ac8e735e37e128e5deba
Midhun Mohan M <84264381+3idhun@users.noreply.github.com>
2025-04-18 13:17:25 +0530

    Update problem_3.py

--- Chapter_11/problem_3.py
+++ Chapter_11/problem_3.py
@@ -8,5 +8,20 @@ Q3. WAP to create a class 'Employee' and add salary increment properties
 
 '''
 
-class Salary:
+class Employee:
+    salary = 234
+    increment = 20
+    @property 
+    def salaryAfterIncrement(self):
+        return(self.salary + self.salary * (self.increment/100))
+        
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        self.increment = ((salary/self.salary) -1)*100
+    
+    
+e = Employee()
+# print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 200
+print(e.increment)
     
