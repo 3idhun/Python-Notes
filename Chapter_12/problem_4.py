8659a4cdb82b7d2ef6e9b9282bd61d50815e66a6
Midhun Mohan M <84264381+3idhun@users.noreply.github.com>
2025-04-22 18:36:56 +0530

    Add files via upload

---
+++ Chapter_12/problem_4.py
@@ -0,0 +1,11 @@
+'''
+
+Q4. WAP to display a/b where a and b are integers, if b=0, display infinite by handling the "ZeroDivisionError".
+
+'''
+try:
+    a = int(input("enter a: "))
+    b = int(input("enter b: "))
+    print(a/b)
+except ZeroDivisionError as v:
+    print("Infinite")
