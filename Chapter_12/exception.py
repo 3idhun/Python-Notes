54c2371cbc61daa2bc96da08ffe5dc27cee2a62d
Midhun Mohan M <84264381+3idhun@users.noreply.github.com>
2025-04-22 14:37:04 +0530

    Create exception.py

---
+++ Chapter_12/exception.py
@@ -0,0 +1,12 @@
+try:
+    a = int (input("Hey, Enter a number:"))
+    print(a)
+    
+except Exception as e:
+    print(e)
+    
+print("Thank You")
+
+'''
+The main purpose of this is not to crash the program in the middle.
+'''
