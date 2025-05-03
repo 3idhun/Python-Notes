768c27bea3a6c25197c3708106af3163b6655c98
3idhun <midhunmohanm2002@gmail.com>
2025-05-03 13:56:42 +0530

    New Learnings.

--- project_jarvis/main.py
+++ project_jarvis/main.py
@@ -1,1 +1,16 @@
-  
+import speechrecognition as sr
+import webbrowser  # inbuilt module
+import pyttsx3  # inbuilt module, text to speech purpose
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+
+def speak(text):
+    engine.say("I will speak this text")
+    engine.runAndWait()
+
+
+if __name__ == "__main__":
+    speak("hey sir how may i help you")
+
